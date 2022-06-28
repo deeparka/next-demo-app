703b4b7f22fe973a7163589722ab64d5439f882f
deeparka <arkadeep180@gmail.com>
2022-06-28 22:46:51 +0530

    added NavBar and styled it

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-    return <div className={styles.container}> Konnichiwa</div>;
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Next JS</title>
+            </Head>
+            <Link href="/about">About</Link>
+            <h1>Konnichiwa</h1>
+        </div>
+    );
 }
